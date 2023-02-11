874c87b10df40f7e7201a3858c3a9e5116460bf5
RiyaadHossain <riyadhossain.dev@gmail.com>
2023-02-11 21:04:51 +0600

    Filtering option added

--- src/pages/AllItems/AllItems.jsx
+++ src/pages/AllItems/AllItems.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import PageBanner from "../../components/PageBanner";
 import bg_img from "../../assets/images/hero-image-2.jpg";
 import { useGetItemsQuery } from "../../features/item/itemAPI";
@@ -6,60 +6,92 @@ import Loading from "../../components/Loading";
 import FoodCard from "../../components/FoodCard";
 
 export default function AllItems() {
+  const [filter, setFilter] = useState({ search: "", category: "" });
   const { data, isFetching } = useGetItemsQuery();
   if (isFetching) return <Loading />;
+
+  const items = data.data.filter((item) => {
+    if (filter.search && filter.category && !filter.category === "All") {
+      const searchText = item.name
+        .toLowerCase()
+        .includes(filter.search.toLocaleLowerCase());
+
+      const categorySelected = item.category === filter.category;
+      if (searchText && categorySelected) {
+        return item;
+      }
+    } else if (filter.search) {
+      if (item.name.toLowerCase().includes(filter.search.toLocaleLowerCase())) {
+        return item;
+      }
+    } else if (filter.category && !filter.category === "All") {
+      if (item.category === filter.category) {
+        return item;
+      }
+    } else {
+      return item;
+    }
+    return null;
+  });
 
   return (
     <div className="pb-10">
       <PageBanner bg_img={bg_img} title="Menu" />
       <div className="pt-16">
-        <div className="mb-20 flex flex-wrap gap-8 md:gap-20 px-6 justify-center">
-          <select className="select rounded-md bg-slate-700 text-white select-bordered w-full max-w-[250px] md:max-w-[300px]">
-            <option selected>Filter by Category</option>
-            <option>BreakFast</option>
-            <option>Lunch</option>
-            <option>Dinner</option>
-          </select>
-          <div className="flex gap-3 flex-wrap ">
-            <select className="select rounded-md bg-slate-700 text-white select-bordered mx-auto w-full max-w-[120px]">
-              <option selected>Sort by</option>
-              <option>BreakFast</option>
-              <option>Lunch</option>
-              <option>Dinner</option>
+        <div className="flex flex-wrap justify-center gap-3">
+          <div className="text-center mb-3 md:mb-0">
+            <select
+              onClick={(e) =>
+                setFilter({ ...filter, category: e.target.value })
+              }
+              defaultValue=""
+              className="select rounded-md bg-slate-700 text-white select-bordered w-full max-w-[250px] md:max-w-[300px]"
+            >
+              <option value="">Filter by Category</option>
+              <option value="BreakFast">BreakFast</option>
+              <option value="Lunch">Lunch</option>
+              <option value="Dinner">Dinner</option>
+              <option value="All">All</option>
             </select>
-            <div className="form-control">
-              <div className="input-group">
-                <input
-                  type="text"
-                  placeholder="Search…"
-                  className="input input-bordered min-w-[310px] md:w-80"
-                />
-                <button className="btn btn-square bg-slate-700">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </button>
-              </div>
+          </div>
+          <div className="form-control mb-8">
+            <div className="input-group justify-center">
+              <input
+                onChange={(e) =>
+                  setFilter({ ...filter, search: e.target.value })
+                }
+                type="text"
+                placeholder="Search…"
+                className="input input-bordered min-w-[310px] md:w-80"
+              />
+              <button className="btn btn-square bg-slate-700">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6 text-white"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </button>
             </div>
           </div>
         </div>
         <div className="flex justify-center flex-wrap gap-8 px-4 md:px-8 lg:px-24">
-          {data.data.map((item) => (
-            <FoodCard item={item} />
-          ))}
+          {items.length ? (
+            items.map((item) => <FoodCard item={item} />)
+          ) : (
+            <span className="text-lg">Ooops! No Items Found 😔</span>
+          )}
         </div>
-        <div className="text-center mt-14">
+
+        {/* <div className="text-center mt-14">
           <div className="btn-group">
             <button className="btn">1</button>
             <button className="btn btn-active">2</button>
@@ -68,7 +100,7 @@ export default function AllItems() {
             <button className="btn">5</button>
             <button className="btn">6</button>
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -13,67 +13,14 @@ export default function Menu() {
     <div className="pb-10">
       <PageBanner bg_img={bg_img} title="Menu" />
       <div className="pt-16">
-        <div className="mb-20 flex flex-wrap gap-8 md:gap-20 px-6 justify-center">
-          <select
-            defaultValue="default"
-            className="select rounded-md bg-slate-700 text-white select-bordered w-full max-w-[250px] md:max-w-[300px]"
-          >
-            <option value="default">Filter by Category</option>
-            <option>BreakFast</option>
-            <option>Lunch</option>
-            <option>Dinner</option>
-          </select>
-          <div className="flex gap-3 flex-wrap ">
-            <select
-              defaultValue="default"
-              className="select rounded-md bg-slate-700 text-white select-bordered mx-auto w-full max-w-[120px]"
-            >
-              <option value="default">Sort by</option>
-              <option>BreakFast</option>
-              <option>Lunch</option>
-              <option>Dinner</option>
-            </select>
-            <div className="form-control">
-              <div className="input-group">
-                <input
-                  type="text"
-                  placeholder="Search…"
-                  className="input input-bordered min-w-[310px] md:w-80"
-                />
-                <button className="btn btn-square bg-slate-700">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 text-white"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </button>
-              </div>
-            </div>
-          </div>
+        <div className="bg-slate-600 text-slate-50 w-10/12 mx-auto h-14 mb-12 flex items-center justify-between px-4 rounded-md font-bold">
+          <h3 className="text-xl">Total Packages</h3>
+          <span className="btn btn-circle btn-sm bg-slate-800 text-slate-50">{data.data.length}</span>
         </div>
         <div className="flex justify-center flex-wrap gap-8 px-4 md:px-8 lg:px-24">
           {data.data.map((item, i) => (
             <PackageCard key={i} item={item} />
           ))}
-        </div>
-        <div className="text-center mt-14">
-          <div className="btn-group">
-            <button className="btn">1</button>
-            <button className="btn btn-active">2</button>
-            <button className="btn">3</button>
-            <button className="btn">4</button>
-            <button className="btn">5</button>
-            <button className="btn">6</button>
-          </div>
         </div>
       </div>
     </div>
